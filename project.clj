081a035e24f2e973282aa0044eb74f1044968ff7
Anders Eliasson <anders.eliasson@digitalroute.com>
2017-02-04 20:27:54 +0100

    refactoring

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject storagemap "0.1.0-SNAPSHOT"
+(defproject storagemap "0.2.0-SNAPSHOT"
   :description "A storage persistent map"
   :url "https://github.com/eliassona/storagemap"
   :license {:name "Eclipse Public License"
